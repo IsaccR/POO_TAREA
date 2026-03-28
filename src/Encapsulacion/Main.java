32d33c5b5653b38c565a91756c3b54b71e5791f7
Your Name <youexample.com>
2026-03-28 07:20:44 -0600

    Persona

---
+++ src/Encapsulacion/Main.java
@@ -0,0 +1,16 @@
+package Encapsulacion;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Persona p1 = new Persona("Juan", "Perez", 20);
+        Persona p2 = new Persona("Maria", "Lopez", 25);
+        Persona p3 = new Persona("Carlos", "Ramirez", 30);
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+
+	}
+
+}
---
+++ src/Encapsulacion/Persona.java
@@ -0,0 +1,18 @@
+package Encapsulacion;
+
+public class Persona {
+	  String nombre;
+	    String apellido;
+	    int edad;
+
+	    public Persona(String nombre, String apellido, int edad) {
+	        this.nombre = nombre;
+	        this.apellido = apellido;
+	        this.edad = edad;
+	    }
+
+	    public String toString() {
+	        return "Nombre: " + nombre + " " + apellido + " Edad: " + edad;
+	    }
+	}
+
